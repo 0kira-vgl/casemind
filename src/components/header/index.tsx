4c4034bff9eb0aef551308de2b19ccd1bb7c50cc
matheus <matheusgtiburcio@gmail.com>
2026-01-26 21:55:47 -0300

    feat: initialize Next.js project with Tailwind setup and base header
    - Initialize Next.js application
    - Configure Tailwind CSS
    - Remove default boilerplate files and folders
    - Add initial header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Logo from "@/assets/icon.svg";
+import { Button } from "../ui/button";
+import { FiMoon } from "react-icons/fi";
+
+export function Header() {
+  return (
+    <div className="border-b-system-muted flex h-24 items-center justify-between border-b-2 px-10 py-5">
+      <div>
+        <Image src={Logo} alt="Image Logo" className="size-20" />
+      </div>
+
+      <div className="flex">
+        <Button variant="link" className="text-base text-zinc-50">
+          Home
+        </Button>
+        <Button variant="link" className="text-base text-zinc-50">
+          Artigos
+        </Button>
+
+        <div className="bg-system-muted mx-5 my-1 flex w-px items-center justify-center" />
+
+        <Button className="mr-5 bg-transparent">
+          <FiMoon />
+        </Button>
+
+        <Button variant="link" className="text-base text-zinc-50">
+          Entrar
+        </Button>
+
+        <Button className="bg-system-primary hover:bg-system-primary-hover rounded-none text-black">
+          Cadastrar
+        </Button>
+      </div>
+    </div>
+  );
+}
